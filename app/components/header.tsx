b9d7fa81170abd95a3ace09f6935b3aed29b7a7b
Antonios Papadopoulos <antonios@smallpdf.com>
2023-06-06 18:45:55 +0200

    update layout structure

---
+++ app/components/header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <header className="h-14 bg-blue-100 flex items-center px-6 justify-between">
+      <h1>Όμιλος Αντισφαίρισης Κέλετρον</h1>
+      <nav>
+        <ul className="flex gap-4">
+          <li>Αρχική</li>
+          <li>Aνακοινώσεις</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
