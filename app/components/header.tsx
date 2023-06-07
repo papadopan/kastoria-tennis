305566e243a57ecf99ff4d6817d936fad5e3a7db
Antonios Papadopoulos <antonios@smallpdf.com>
2023-06-07 16:49:49 +0200

    add border to header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="p-5 bg-blue-100 flex items-center px-6 justify-end">
+    <header className="p-5  flex items-center px-6 justify-end border-b">
       <nav>
         <ul className="flex gap-4">
           <li>Αρχική</li>
