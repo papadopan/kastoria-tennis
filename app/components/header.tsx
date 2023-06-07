4b95422eec9be1a5d0af78a06768ee3c31e1d6b4
Antonios Papadopoulos <antonios@smallpdf.com>
2023-06-07 20:40:10 +0200

    link new pages from header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="p-5  flex items-center px-6 justify-end border-b">
       <nav>
         <ul className="flex gap-4">
-          <li>Αρχική</li>
-          <li>Aνακοινώσεις</li>
+          <Link href="/">
+            <li>Αρχική</li>
+          </Link>
+          <Link href="/posts">
+            <li>Ανακοινώσεις</li>
+          </Link>
         </ul>
       </nav>
     </header>
