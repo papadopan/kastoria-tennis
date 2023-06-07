f57b986a839572e01f2333e1dc953dbfb8fa9b8e
Antonios Papadopoulos <antonios@smallpdf.com>
2023-06-07 19:22:02 +0200

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-green-600 p-10 flex justify-around flex-col md:flex-row gap-16">
+      <div>
+        <p>Ακαδημία Αντισφαίρισης &quot;Κελετρον&quot;</p>
+        <p>Λεωφόρος Γουναράδων Ε.ΔΗ.ΚΑ</p>
+        <p>Περιοχή Χλόη</p>
+        <p>Καστοριά 52100 </p>
+      </div>
+      <div>social</div>
+    </footer>
+  );
+};
