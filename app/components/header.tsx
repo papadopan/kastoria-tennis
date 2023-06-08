88e0362169e94d5b1d800a3fc015880343776991
Antonios Papadopoulos <antonios@smallpdf.com>
2023-06-08 10:20:39 +0200

    add logo to the header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="p-5  flex items-center px-6 justify-end border-b">
+    <header className="p-5  flex items-center px-6 justify-between border-b">
+      <Link href={"/"}>
+        <Image
+          src="/logo.png"
+          alt="Λογότυπο ΕΠΑΛ Καρπενησίου"
+          width={64}
+          height={64}
+        />
+      </Link>
       <nav>
         <ul className="flex gap-4">
           <Link href="/">
