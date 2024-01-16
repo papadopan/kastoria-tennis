f90546d17f8ddf6c325ee2f57b8b8d4aec112bd8
Antonios Papadopoulos <antonios.papadopoulos@nexthink.com>
2024-01-16 22:17:43 +0100

    Add Analytics component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Header } from "./components/header";
 import { Footer } from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         <main className="min-h-[90vh] p-6 md:p-10">{children}</main>
+        <Analytics />
         <Footer />
       </body>
     </html>
